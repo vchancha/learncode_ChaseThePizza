709235ff9697a7fc22c921a8fc0701645a9b83b3
vchancha <93780071+vchancha@users.noreply.github.com>
2023-02-25 00:29:33 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    Pizza.setPosition(randint(20, 140), randint(20, 100))
+    info.startCountdown(3)
+})
+let Pizza: Sprite = null
+scene.setBackgroundColor(9)
+let Hero = sprites.create(img`
+    ..............ccccccccc........
+    ............cc555555555cc......
+    ...........c5555555555555c.....
+    ..........c55555555555555dc....
+    .........c555555555555b5bdc....
+    .........555bc1555555555bdcccc.
+    ........c555ccc55555555bbdccddc
+    ........c555bcb5555555ccddcdddc
+    .......c555555555551ccccddbdddc
+    .......c555555b555c1cccbddbbdbc
+    .......c5555555bbc33333ddddbcc.
+    .......c555555555bc333555ddbc..
+    .......c5555555555555555555c...
+    .......cd555555555555cccc555c..
+    .......cd55555555555c555c555c..
+    .......cdd555555555b5555b555c..
+    .......cddd55555ddbb555bb555c..
+    .......cdddd55555555555b5555c..
+    .......cddddd5555555ddb5555dc..
+    c......cdddddd555555555555dcc..
+    cc...ccddddddd555555555555dc...
+    cdccccdddddd555555d55555ddcc...
+    cdddddddddbd5555555ddddddccccc.
+    ccdddddddbb55555555bddddccbddc.
+    .ccddddddbd55555555bdddccdddc..
+    ..cccddddbd5555555cddcccddbc...
+    ....ccccccd555555bcccc.cccc....
+    .........cc555555bc............
+    .........cc55555555c...........
+    ..........cccccccccc...........
+    `, SpriteKind.Player)
+controller.moveSprite(Hero)
+Pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+info.startCountdown(10)
